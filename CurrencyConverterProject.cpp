2ea00b032ca9ade0a140b38a0841d30a4f073541
Mohammed Tabrez Pasha <122313213+mohammedtabrezpasha1994@users.noreply.github.com>
2023-02-19 03:28:12 +0530

    Add files via upload

---
+++ CurrencyConverterProject.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+int main()
+{      //Declaration of varibles
+       int dollars;
+       float B,M,J,I,U,S,K,Q,O,A,N,C,T,R,P,L;
+       //input united states dollars
+       cout<<"Enter currency in United States Dollars"<<endl;
+       cin>>dollars;
+       //Calculations
+       B=dollars*0.83;
+       M=dollars*18.36;
+       J=dollars*134.12;
+       I=dollars*82.77;
+       U=dollars*3.67;
+       S=dollars*3.75;
+       K=dollars*0.31;
+       Q=dollars*3.64;
+       O=dollars*0.38;
+       A=dollars*1.45;
+       N=dollars*1.60;
+       A=dollars*1.45;
+       C=dollars*6.87;
+       T=dollars*34.45;
+       R=dollars*74.00;
+       P=dollars*260.40;
+       L=dollars*157.30;
+
+       cout<<dollars<<"United States Dollars are equal to "<<B<<" British Pounds"<<endl;
+       cout<<dollars<<"United States Dollars are equal to "<<M<<" Mexican Pesos"<<endl;
+       cout<<dollars<<"United States Dollars are equal to "<<J<<" Japanese Yen"<<endl;
+       cout<<dollars<<"United States Dollars are equal to "<<I<<" Indian Rupees"<<endl;
+       cout<<dollars<<"United States Dollars are equal to "<<U<<" UAE Dirham"<<endl;
+       cout<<dollars<<"United States Dollars are equal to "<<S<<" Saudi Riyal"<<endl;
+       cout<<dollars<<"United States Dollars are equal to "<<K<<" Kuwaiti Dinar"<<endl;
+       cout<<dollars<<"United States Dollars are equal to "<<Q<<" Qatari Riyal"<<endl;
+       cout<<dollars<<"United States Dollars are equal to "<<O<<" Omani Rial"<<endl;
+       cout<<dollars<<"United States Dollars are equal to "<<A<<" Australian Dollar"<<endl;
+       cout<<dollars<<"United States Dollars are equal to "<<N<<" NewZealand Dollar"<<endl;
+       cout<<dollars<<"United States Dollars are equal to "<<C<<" Chinese Yuan"<<endl;
+       cout<<dollars<<"United States Dollars are equal to "<<T<<" Thai Baht"<<endl;
+       cout<<dollars<<"United States Dollars are equal to "<<N<<" NewZealand Dollar"<<endl;
+       cout<<dollars<<"United States Dollars are equal to "<<R<<" Russian Ruble"<<endl;
+       cout<<dollars<<"United States Dollars are equal to "<<P<<" Pakistani Rupees"<<endl;
+       cout<<dollars<<"United States Dollars are equal to "<<L<<" Liberian Dollar"<<endl;
+
+
+
+       system("pause");
+}
